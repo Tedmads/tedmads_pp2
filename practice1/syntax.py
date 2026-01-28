2c581972d433be3bf1105a05ac44752c323993ee
Tedmads <m_taidzhuma@kbtu.kz>
2026-01-28 22:33:38 +0500

    syntax.py

--- practice1/syntax.py
+++ practice1/syntax.py
@@ -1,1 +1,14 @@
+print('Hello world')
 
+if 1>0:
+  print("Salem alem")
+
+x=10
+x="salem"
+print(x)
+
+a="Madina"
+b=17
+print("Salem, menin esimim" a, b 'zhastamyn')
+
+print("Seni suiy ushin magan",24 "sagat ta az bolady")
