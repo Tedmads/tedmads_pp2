360b38d4257a426b11e893f7486ce82edd98f3eb
Tedmads <m_taidzhuma@kbtu.kz>
2026-02-04 00:51:28 +0500

    Update casting.py

--- practice1/casting.py
+++ practice1/casting.py
@@ -1,1 +1,14 @@
+first=int(90.90995) 
+#first`ll be 90
 
+second=int("271") 
+#second will be 271
+
+third=float(2)   
+#third will be 2.0
+
+fourth=float("25")   
+#fourth will be 25.0
+
+fifth=str(3.0)  
+#fifth will be '3.0'
