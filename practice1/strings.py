c6215def82a1dcc15894c7ea475123bd9f8550ba
Tedmads <m_taidzhuma@kbtu.kz>
2026-02-04 00:59:54 +0500

    strings.py

--- practice1/strings.py
+++ practice1/strings.py
@@ -1,1 +1,18 @@
+print("They call him 'Professor'")
 
+second='''Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua.'''
+print(second)
+
+third="Salem Alem!"
+print(third[:5])
+
+fourth="Salem Alem!"
+print(fourth.split()) 
+#returns: ['Salem', ' Alem!']
+
+Pri=10
+fifth=f"The price is {20 * Pri} dollars"
+print(fifth)
