c388e075b8dcf2a335fa5b36d5496ada095bbe2a
Tedmads <m_taidzhuma@kbtu.kz>
2026-02-07 16:39:22 +0500

    oper

---
+++ prac2/operators.py
@@ -0,0 +1,20 @@
+"""
++   Addition	x + y	
+-	Subtraction	x - y	
+*	Multiplication	x * y	
+/	Division	x / y	
+%	Modulus	x % y	
+**	Exponentiation	x ** y	
+//	Floor division	x // y
+"""
+print(10+5)
+
+print((6+3)-(6+3))
+
+print(10/2)
+
+if 5!=10:
+  print("5 and 10 is not equal")
+
+if 5==10 or 4==4:
+  print("At least one of the statements is true")
